848d07aacd897864b3362d7966b8c06502d0ec0c
rubenacevedo07 <rubenacevedo07@gmail.com>
2020-09-08 14:23:13 +0200

    Mongo Atlas

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,13 @@
-dbPassword = 'mongodb://localhost:27017/mydb01';
+//dbPassword = 'mongodb://ra_dbuser_008:ooyrZsIbkVRjb3C3@cluster0.kv4it.gcp.mongodb.net/Passport?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://ra_dbuser_008:hkKlFlxnGjg4Ymog@cluster0.kv4it.gcp.mongodb.net/Passport?retryWrites=true&w=majority';
+/*const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://ra_dbuser_008:<password>@cluster0.kv4it.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});*/
 
 module.exports = {
     mongoURI: dbPassword
